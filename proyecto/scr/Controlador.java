923b40c07e24f764a2d60624648aee65ab2fe962
Matias Rojas Comas <matrojas22@alumnos.utalca.cl>
2024-08-29 17:44:01 -0400

    Se agrego el actualizar protoboard, que actualiza cada fila y columna correspondiente dependiendo de la energia que tenga cada una

--- proyecto/scr/Controlador.java
+++ proyecto/scr/Controlador.java
@@ -56,4 +56,56 @@ public class Controlador {
             }
         }
     }
+    public void ActualizarProtoboard(GridPane gridPane) {
+        // Actualizar filas del 2 al 6
+        for (int i = 2; i < 7; i++) {
+            for (int j = 0; j < 30; j++) {
+                Circle punto = (Circle) gridPane.getChildren().get(i * 30 + j);
+                if (punto.getFill() == Color.GREEN) {
+                    for (int m = 2; m < 7; m++) {
+                        Circle punto2 = (Circle) gridPane.getChildren().get(m * 30 + j);
+                        punto2.setFill(Color.GREEN);
+                    }
+                }
+            }
+        }
+        
+        // Actualizar filas del 7 al 12
+        for (int i = 7; i < 12; i++) {
+            for (int j = 0; j < 30; j++) {
+                Circle punto = (Circle) gridPane.getChildren().get(i * 30 + j);
+                if (punto.getFill() == Color.GREEN) {
+                    for (int m = 7; m < 12; m++) {
+                        Circle punto2 = (Circle) gridPane.getChildren().get(m * 30 + j);
+                        punto2.setFill(Color.GREEN);
+                    }
+                }
+            }
+        }
+        // Actualizar filas del 2 al 6
+        for (int i = 2; i < 7; i++) {
+            for (int j = 0; j < 30; j++) {
+                Circle punto = (Circle) gridPane.getChildren().get(i * 30 + j);
+                if (punto.getFill() == Color.RED) {
+                    for (int m = 2; m < 7; m++) {
+                        Circle punto2 = (Circle) gridPane.getChildren().get(m * 30 + j);
+                        punto2.setFill(Color.RED);
+                    }
+                }
+            }
+        }
+        
+        // Actualizar filas del 7 al 12
+        for (int i = 7; i < 12; i++) {
+            for (int j = 0; j < 30; j++) {
+                Circle punto = (Circle) gridPane.getChildren().get(i * 30 + j);
+                if (punto.getFill() == Color.RED) {
+                    for (int m = 7; m < 12; m++) {
+                        Circle punto2 = (Circle) gridPane.getChildren().get(m * 30 + j);
+                        punto2.setFill(Color.RED);
+                    }
+                }
+            }
+        }
+    }
 }
