a852bbe4b9aec23330ba2fdeca06e56437744c27
rama6832 <ramitaxd05@gmail.com>
2024-08-31 19:14:29 -0400

    se subieron los archivos equivocados , aparte se mejoro el fondo
    Signed-off-by: rama6832 <ramitaxd05@gmail.com>

--- proyecto/scr/Cable.java
+++ proyecto/scr/Cable.java
@@ -1,28 +1,25 @@
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Line;
+import javafx.scene.image.ImageView;
 
 public class Cable {
     private final Line linea;
     private final ImageView imageView;
-    private final Cablear cablear; 
-    private static final double GROSOR_CABLE = 4; // Ajusta el grosor 
 
-    public Cable(double startX, double startY, double endX, double endY, Paint color, ImageView imageView, Cablear cablear) {
+    public Cable(double startX, double startY, double endX, double endY, Paint color, ImageView imageView) {
         this.linea = new Line(startX, startY, endX, endY);
         this.linea.setStroke(color);
-        this.linea.setStrokeWidth(GROSOR_CABLE); // Establece el grosor de la línea
+        this.linea.setStrokeWidth(4); // Ajusta el grosor aquí
 
         this.imageView = imageView;
-        this.cablear = cablear; 
 
-        // Añadir el evento de clic al cable para eliminarlo
-        this.linea.setOnMouseClicked(this::eliminarCable);
-    }
-
-    private void eliminarCable(MouseEvent event) {
-        cablear.eliminarCable(this); // Llamar al método de Cablear para eliminar el cable
+        // Asegúrate de que el cable pueda detectar clics
+        this.linea.setOnMouseClicked(event -> {
+            if (event.getButton() == MouseButton.SECONDARY) { // Detecta clic derecho
+                event.consume(); // Evita que el clic se propague
+            }
+        });
     }
 
     public Line getLinea() {
