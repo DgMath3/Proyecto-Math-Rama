1a058853a0610604bbe2f30ae829806684ac4494
rama6832 <ramitaxd05@gmail.com>
2024-08-31 17:01:17 -0400

    se agrego la clase cable para trabajar mejor la clase cablear / se añadió la función para borrar cables
    Signed-off-by: rama6832 <ramitaxd05@gmail.com>

--- proyecto/scr/Bateria.java
+++ proyecto/scr/Bateria.java
@@ -5,35 +5,33 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
-//crear clase bateria la cual usara la imagen de resources/bateria.jpg para mostrarla a la derecha del gridpane del protoboard con dos botones, uno arriba de color verde y otro abajo de color rojo
-public class Bateria{
+public class Bateria {
     private VBox contenedorBateria;
     public ImageView bateriaImagen;
     public Button botonRojo;
     public Button botonVerde;
 
-    public Bateria(){
+    public Bateria() {
         contenedorBateria = new VBox();
-        Image bateriaImagen = new Image("file:C:\\Users\\Felipe\\Desktop\\proyecto\\resources\\bateria.png"); 
-
-
-        ImageView bateriaImageView = new ImageView(bateriaImagen); 
-        bateriaImageView.setFitWidth(100); 
-        bateriaImageView.setPreserveRatio(true); 
-        Button botonVerde = new Button("+");
+        bateriaImagen = new ImageView(new Image("file:C:\\Users\\Felipe\\Desktop\\proyecto\\resources\\bateria.png")); 
+        bateriaImagen.setFitWidth(100); 
+        bateriaImagen.setPreserveRatio(true); 
+        
+        botonVerde = new Button("+");
         botonVerde.setStyle("-fx-background-color: green; -fx-text-fill: white;");
-        Button botonRojo = new Button("-");
+        
+        botonRojo = new Button("-");
         botonRojo.setStyle("-fx-background-color: red; -fx-text-fill: white;");
+        
+        contenedorBateria.getChildren().addAll(botonVerde, bateriaImagen, botonRojo);
 
-
-        contenedorBateria.getChildren().addAll(botonVerde,bateriaImageView,botonRojo);
-
-    contenedorBateria.setAlignment(Pos.CENTER);
-    contenedorBateria.setSpacing(10);
-    contenedorBateria.setPadding(new Insets(10));
+        contenedorBateria.setAlignment(Pos.CENTER);
+        contenedorBateria.setSpacing(10);
+        contenedorBateria.setPadding(new Insets(10));
     }
 
-    public VBox getContenedorBateria(){
+    public VBox getContenedorBateria() {
         return contenedorBateria;
     }
+    
 }
--- proyecto/scr/Main.java
+++ proyecto/scr/Main.java
@@ -1,68 +1,84 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.stage.Stage;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        // Crear los componentes
+        // Crear los componentes principales
         Protoboard protoboard = new Protoboard();
         Bateria bateria = new Bateria();
         Loc loc = new Loc(protoboard.getGridPane(), Color.BLACK, null);
-        Cablear cablear = new Cablear(protoboard.getGridPane(), loc);
-        MenuBarra menuBarra = new MenuBarra(cablear);
-        Controlador controlador = new Controlador();
-
-        // Cargar la imagen de fondo
-        Image fondoImagen = new Image("file:C:\\Users\\Felipe\\Desktop\\proyecto\\resources\\fondo.png"); // Asegúrate de que la ruta sea correcta
-        ImageView fondoImageView = new ImageView(fondoImagen);
-        fondoImageView.setFitWidth(920);  // Ajustar el tamaño al de la escena
-        fondoImageView.setFitHeight(600);
-
-        // Crear un contenedor StackPane para poner el fondo
-        StackPane fondoPane = new StackPane();
-        fondoPane.getChildren().add(fondoImageView);
-        
-        // Crear el contenedor BorderPane para los componentes
-        BorderPane root = new BorderPane();
-        root.setTop(menuBarra.getMenuBar());
-        root.setCenter(protoboard.getGridPane());
-        root.setRight(bateria.getContenedorBateria());
-
-        // Añadir el contenedor BorderPane al StackPane
-        fondoPane.getChildren().add(root);
 
         // Crear la escena
-        Scene scene = new Scene(fondoPane, 920, 600);
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("Simulador de Protoboard");
+        StackPane root = new StackPane();
+        Scene scene = new Scene(root, 1200, 800); // Ajusta el tamaño según tus necesidades
+        
+        // Crear la instancia de Cablear con la escena
+        Cablear cablear = new Cablear(protoboard.getGridPane(), loc);
+        
+        // Crear la barra de menú con la instancia de Cablear
+        MenuBarra menuBarra = new MenuBarra(cablear);
+        
+        // Crear el controlador
+        Controlador controlador = new Controlador();
+
+        // Crear la imagen de fondo
+        Image fondoImagen = new Image("file:C:\\Users\\Felipe\\Desktop\\proyecto\\resources\\fondo.png");
+        BackgroundSize backgroundSize = new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, true);
+        BackgroundImage backgroundImage = new BackgroundImage(fondoImagen, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background fondo = new Background(backgroundImage);
+        root.setBackground(fondo);
+
+        // Crear un VBox para organizar los componentes verticalmente
+        VBox mainLayout = new VBox();
+        mainLayout.setAlignment(Pos.TOP_CENTER);
+        mainLayout.setSpacing(20);
+        mainLayout.setPadding(new Insets(10, 20, 10, 20));
+
+        // Añadir la barra de menú
+        mainLayout.getChildren().add(menuBarra.getMenuBar());
+
+        // Crear un HBox para organizar el protoboard y la batería horizontalmente
+        HBox contentLayout = new HBox();
+        contentLayout.setAlignment(Pos.CENTER);
+        contentLayout.setSpacing(50); // Espacio entre el protoboard y la batería
+
+        // Añadir el protoboard y la batería al HBox
+        contentLayout.getChildren().addAll(protoboard.getGridPane(), bateria.getContenedorBateria());
+
+        // Añadir el HBox al VBox principal
+        mainLayout.getChildren().add(contentLayout);
+
+        // Añadir el VBox al StackPane raíz
+        root.getChildren().add(mainLayout);
 
         // Configurar los botones de la batería
         bateria.botonVerde.setOnAction(e -> {
-        loc.getFilaActual();
-        loc.getColumnaActual();
-        protoboard.cambiarColor(loc.getFilaActual(), loc.getColumnaActual(), Color.GREEN);
-        controlador.actualizarBuses(protoboard.getGridPane()); // Actualizar buses después de cambiar el color
-        controlador.ActualizarProtoboard(protoboard.getGridPane()); // Actualizar protoboard después de cambiar el color
+            loc.getFilaActual();
+            loc.getColumnaActual();
+            protoboard.cambiarColor(loc.getFilaActual(), loc.getColumnaActual(), Color.GREEN);
+            controlador.actualizarBuses(protoboard.getGridPane()); // Actualizar buses después de cambiar el color
+            controlador.ActualizarProtoboard(protoboard.getGridPane()); // Actualizar protoboard después de cambiar el color
         });
         bateria.botonRojo.setOnAction(e -> {
-        loc.getFilaActual();
-        loc.getColumnaActual();
-        protoboard.cambiarColor(loc.getFilaActual(), loc.getColumnaActual(), Color.RED);
-        controlador.actualizarBuses(protoboard.getGridPane()); // Actualizar buses después de cambiar el color
-        controlador.ActualizarProtoboard(protoboard.getGridPane()); // Actualizar protoboard después de cambiar el color
+            loc.getFilaActual();
+            loc.getColumnaActual();
+            protoboard.cambiarColor(loc.getFilaActual(), loc.getColumnaActual(), Color.RED);
+            controlador.actualizarBuses(protoboard.getGridPane()); // Actualizar buses después de cambiar el color
+            controlador.ActualizarProtoboard(protoboard.getGridPane()); // Actualizar protoboard después de cambiar el color
         });
 
         // Configurar el Stage
-        primaryStage.setResizable(false); // Desactiva el redimensionamiento de la ventana
-        primaryStage.setFullScreen(false); // Asegura que el modo de pantalla completa está desactivado
-
+        primaryStage.setTitle("Simulador de Protoboard");
+        primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
